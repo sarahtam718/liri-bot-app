2973b29b0707fd6e96d3d25ef2df8a553ca51b19
Sarah Tam <sarahtam718@gmail.com>
2019-03-24 17:15:33 -0500

    finished bands function and working on spotify function

--- liri.js
+++ liri.js
@@ -14,7 +14,18 @@ var axios = require("axios");
 var figlet = require('figlet');
 
 // access spotify keys info
-var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify)
+console.log(keys.spotify)
+
+// cool title
+figlet('LiRi Node App', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
 
 // user command choice
 // * `concert-this`
@@ -26,3 +37,46 @@ var spotify = new Spotify(keys.spotify);
 //     * `do-what-it-says`
 
 // check for user command (if-else or switch) - 4 options, run your own function to pull data
+
+// BANDS in TOWN function
+function getMyBand(artists) {
+    var queryURL = 'https://rest.bandsintown.com/artists/' + artists + '/events?app_id=codingbootcamp';
+    axios.get(queryURL)
+        .then(function (response) {
+            // console.log("Upcoming concerts for: ", artists);
+            // console.log("Drake data: ", response);
+            for (let i = 0; i < response.data.length; i++) {
+                var show = response.data[i];
+                console.log("Name: ", show.venue.name);
+                console.log("Location: ", show.venue.city || show.venue.region || show.venue.country);
+                console.log("Date: ", moment(show.datetime, "YYYY-MM-DD").format("MM/DD/YYYY"));
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+// getMyBand("Drake");
+
+// function getSpotify(song) {
+//     spotify
+//         .search({ type: 'track', query: song })
+//         .then(function (response) {
+//             console.log("My song: ", response.data);
+//         })
+//         .catch(function (err) {
+//             console.log(err);
+//         });
+// }
+
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, response) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+
+    console.log(response);
+});
+
+// getSpotify("Happy");
