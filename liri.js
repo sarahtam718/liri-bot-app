7ac5a18f9d6a2b85910001e3fc4bc8e07142b9ef
Sarah Tam <sarahtam718@gmail.com>
2019-03-24 16:06:47 -0500

    added required packages

--- liri.js
+++ liri.js
@@ -1,15 +1,28 @@
-// pseudocode
-
-
 // require .env *AT THE TOP*
-// require("dotenv").config();
+require("dotenv").config();
 
 // require keys.js
-// var keys = require("./keys.js");
+var keys = require("./keys.js");
 
+// require spotify, axios, moment, inquirer, fs (to access random.txt text)
+var Spotify = require("node-spotify-api");
+var inquirer = require("inquirer");
+var moment = require("moment");
+var fs = require("fs");
+var axios = require("axios");
+// cool title package...
+var figlet = require('figlet');
 
-// require spotify, axios, moment, inquirer
+// access spotify keys info
+var spotify = new Spotify(keys.spotify);
 
 // user command choice
+// * `concert-this`
+
+//     * `spotify-this-song`
+
+//     * `movie-this`
+
+//     * `do-what-it-says`
 
 // check for user command (if-else or switch) - 4 options, run your own function to pull data
