4ff1d7f51feb1617f8fccf98a4937489aabb71f3
Sarah Tam <sarahtam718@gmail.com>
2019-03-24 15:38:32 -0500

    intitial files, set up, download of packages

---
+++ liri.js
@@ -0,0 +1,18 @@
+// pseudocode
+
+// get spotify API key
+
+// install packages
+
+// require .env *AT THE TOP*
+// require("dotenv").config();
+
+// require keys.js
+// var keys = require("./keys.js");
+
+
+// require spotify, axios, moment, inquirer
+
+// user command choice
+
+// check for user command (if-else or switch) - 4 options, run your own function to pull data
